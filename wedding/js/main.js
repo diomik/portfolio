7ca48726f6e14ccf4ef7fbcea863c65e558c6147
Vadim Vedenev <44378562+diomik@users.noreply.github.com>
2019-04-02 23:19:44 +0300

    Add files via upload

---
+++ wedding/js/main.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        dots: true,
+        items: 1,
+        loop:true,
+        margin:30,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+
+
+
+
+    });
+});
